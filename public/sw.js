091017bf34a4dd87f42d7483e01a18c7f4401d3a
mentalx4t-cmd <mentalx4t@gmail.com>
2026-05-12 12:12:44 +0500

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+// Basic Service Worker for PWA installability
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  // Pass-through strategy for now
+  event.respondWith(fetch(event.request));
+});
